658335191fcda9ca843302dd625b4135b957aa31
Mike Trilford <trilfordm@gmail.com>
2025-06-02 17:22:33 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// public/service-worker.js
+self.addEventListener("install", () => {
+  console.log("Service worker installed.");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service worker activated.");
+});
