85fa0ae2d00cfd97c1ca3033b19c039b5bfddf6a
Mike Trilford <trilfordm@gmail.com>
2025-06-11 16:18:24 +1000

    Added component group names

--- src/muibook/navbar/navbar.js
+++ src/muibook/navbar/navbar.js
@@ -67,6 +67,17 @@ class appNavbar extends HTMLElement {
         text-decoration: underline;
       }
 
+
+      mui-body::part(color) {
+        color: var(--app-nav-accent);
+      }
+      mui-body::part(margin) {
+        margin-top: var(--space-400);
+        margin-bottom: var(--space-200);
+      }
+      mui-body.first::part(margin) {
+        margin-top: var(--space-000);
+      }
     `;
 
     const Home = /*html*/ `
@@ -107,36 +118,48 @@ class appNavbar extends HTMLElement {
 
     const Components = /*html*/ `
       <app-navbar-group id="web-components" groupname="Components">
-        <app-navbar-link link="#/smart-card" title="Smart Card"></app-navbar-link>   
-        <app-navbar-link link="#/tab-bar" title="Tab Bar"></app-navbar-link>  
-        <app-navbar-link link="#/carousel" title="Carousel"></app-navbar-link>  
+        <mui-body class="first" weight="bold">Inputs</mui-body>
         <app-navbar-link link="#/field" title="Field"></app-navbar-link> 
         <app-navbar-link link="#/file-upload" title="File Upload"></app-navbar-link> 
         <app-navbar-link link="#/addon" title="Add On"></app-navbar-link>   
         <app-navbar-link link="#/input" title="Input"></app-navbar-link>  
         <app-navbar-link link="#/select" title="Select"></app-navbar-link>  
         <app-navbar-link link="#/switch" title="Switch"></app-navbar-link>  
+        <mui-body weight="bold">Content</mui-body>
+        <app-navbar-link link="#/heading" title="Heading"></app-navbar-link>
+        <app-navbar-link link="#/body" title="Body"></app-navbar-link>
+        <app-navbar-link link="#/list" title="List"></app-navbar-link>
+        <app-navbar-link link="#/quote" title="Quote"></app-navbar-link>
+        <app-navbar-link link="#/code" title="Code"></app-navbar-link>
+        <app-navbar-link link="#/image" title="Image"></app-navbar-link>
+        <app-navbar-link link="#/smart-card" title="Smart Card"></app-navbar-link>   
+        <app-navbar-link link="#/icons" title="Icons"></app-navbar-link>
+        <mui-body weight="bold">Layout</mui-body>
+        <app-navbar-link link="#/stack" title="Stack"></app-navbar-link>
+        <app-navbar-link link="#/grid" title="Grid"></app-navbar-link>
+        <app-navbar-link link="#/responsive" title="Responsive"></app-navbar-link>
+        <app-navbar-link link="#/container" title="Container"></app-navbar-link>
+        <app-navbar-link link="#/rule" title="Rule"></app-navbar-link>
+        <app-navbar-link link="#/card" title="Card"></app-navbar-link>
+
+        <mui-body weight="bold">Data Display</mui-body>
+        <app-navbar-link link="#/table" title="Table"></app-navbar-link>
+        <app-navbar-link link="#/slat" title="Slat"></app-navbar-link>
+        <app-navbar-link link="#/accordion" title="Accordion"></app-navbar-link>
+        
+        <mui-body weight="bold">Feedback</mui-body>
         <app-navbar-link link="#/alert" title="Alert"></app-navbar-link>
         <app-navbar-link link="#/message" title="Message"></app-navbar-link>
         <app-navbar-link link="#/badge" title="Badge"></app-navbar-link>
+        
+        <mui-body weight="bold">Actions</mui-body>
         <app-navbar-link link="#/button" title="Button"></app-navbar-link>
         <app-navbar-link link="#/link" title="Link"></app-navbar-link>
-        <app-navbar-link link="#/responsive" title="Responsive"></app-navbar-link>
-        <app-navbar-link link="#/stack" title="Stack"></app-navbar-link>
-        <app-navbar-link link="#/grid" title="Grid"></app-navbar-link>
-        <app-navbar-link link="#/card" title="Card"></app-navbar-link>
-        <app-navbar-link link="#/container" title="Container"></app-navbar-link>
-        <app-navbar-link link="#/heading" title="Heading"></app-navbar-link>
-        <app-navbar-link link="#/body" title="Body"></app-navbar-link>
-        <app-navbar-link link="#/image" title="Image"></app-navbar-link>
-        <app-navbar-link link="#/list" title="List"></app-navbar-link>
-        <app-navbar-link link="#/quote" title="Quote"></app-navbar-link>
-        <app-navbar-link link="#/rule" title="Rule"></app-navbar-link>
-        <app-navbar-link link="#/icons" title="Icons"></app-navbar-link>
-        <app-navbar-link link="#/code" title="Code"></app-navbar-link>
-        <app-navbar-link link="#/accordion" title="Accordion"></app-navbar-link>
-        <app-navbar-link link="#/table" title="Table"></app-navbar-link>
-        <app-navbar-link link="#/slat" title="Slat"></app-navbar-link>
+        
+        <mui-body weight="bold">Navigation</mui-body>
+        <app-navbar-link link="#/tab-bar" title="Tab Bar"></app-navbar-link>  
+        <app-navbar-link link="#/carousel" title="Carousel"></app-navbar-link>  
+        
       </app-navbar-group>
     `;
 
