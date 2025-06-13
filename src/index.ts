ad2349f480f8e0fce970fbd76f72b51d0ffa41ba
Mike Trilford <trilfordm@gmail.com>
2025-06-13 11:56:50 +1000

    Discovered and fixed component export issues

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,31 @@
-// CORE
-
 // Utils
 export * from "./utils/part-map";
 
-// Forms
-export * from "./components/mui-addon";
+// Inputs
 export * from "./components/mui-field";
+export * from "./components/mui-file-upload";
+export * from "./components/mui-addon";
 export * from "./components/mui-input";
 export * from "./components/mui-select";
 export * from "./components/mui-switch";
-export * from "./components/mui-file-upload";
 
-// Typography
-export * from "./components/mui-card/header";
-export * from "./components/mui-card/body";
-export * from "./components/mui-list/item";
-export * from "./components/mui-list/list";
-export * from "./components/mui-quote";
-
-// Feedback
-export * from "./components/mui-alert";
-export * from "./components/mui-message";
-export * from "./components/mui-badge";
-
-// Actions
-export * from "./components/mui-button";
-export * from "./components/mui-button-group";
-export * from "./components/mui-link";
-
-// Layout
-export * from "./components/mui-grid";
-export * from "./components/mui-stack/hstack";
-export * from "./components/mui-stack/vstack";
-export * from "./components/mui-container";
-export * from "./components/mui-responsive";
-export * from "./components/mui-rule";
-
-// Data
+// Content
 export * from "./components/mui-table/table";
 export * from "./components/mui-table/cell";
 export * from "./components/mui-table/row";
 export * from "./components/mui-table/row-group";
 export * from "./components/mui-slat";
-export * from "./components/mui-tabs/controller";
-export * from "./components/mui-tabs/item";
-export * from "./components/mui-tabs/panel";
-export * from "./components/mui-tabs/tab-bar";
-export * from "./components/mui-image";
-
-// Code
+export * from "./components/mui-accordion/group";
+export * from "./components/mui-accordion/inline";
+export * from "./components/mui-accordion/block";
+export * from "./components/mui-heading";
+export * from "./components/mui-body";
+export * from "./components/mui-list/item";
+export * from "./components/mui-list/list";
+export * from "./components/mui-quote";
 export * from "./components/mui-code";
-
-// Card
-export * from "./components/mui-card/card";
-export * from "./components/mui-card/header";
-export * from "./components/mui-card/body";
-export * from "./components/mui-card/footer";
-
-// Icons
+export * from "./components/mui-image";
+export * from "./components/mui-smart-card";
 export * from "./components/mui-icons/accessibility";
 export * from "./components/mui-icons/add";
 export * from "./components/mui-icons/attention";
@@ -82,16 +50,32 @@ export * from "./components/mui-icons/up-arrow";
 export * from "./components/mui-icons/up-chevron";
 export * from "./components/mui-icons/warning";
 
-// ADVANCED
+// Layout
+export * from "./components/mui-stack/hstack";
+export * from "./components/mui-stack/vstack";
+export * from "./components/mui-grid";
+export * from "./components/mui-responsive";
+export * from "./components/mui-container";
+export * from "./components/mui-rule";
+export * from "./components/mui-card/card";
+export * from "./components/mui-card/header";
+export * from "./components/mui-card/body";
+export * from "./components/mui-card/footer";
 
-// Accordion
-export * from "./components/mui-accordion/group";
-export * from "./components/mui-accordion/inline";
-export * from "./components/mui-accordion/block";
+// Feedback
+export * from "./components/mui-alert";
+export * from "./components/mui-message";
+export * from "./components/mui-badge";
 
-// Carousel
+// Actions
+export * from "./components/mui-button";
+export * from "./components/mui-button-group";
+export * from "./components/mui-link";
+
+// Navigation
+export * from "./components/mui-tabs/controller";
+export * from "./components/mui-tabs/item";
+export * from "./components/mui-tabs/panel";
+export * from "./components/mui-tabs/tab-bar";
 export * from "./components/mui-carousel/controller";
 export * from "./components/mui-carousel/panel";
-
-// Smart Card
-export * from "./components/mui-smart-card";
