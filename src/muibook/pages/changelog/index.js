c23d856fdbc76fc42e23561776e22b91a6d1ce9e
Michael Trilford <trilfordm@gmail.com>
2025-09-21 12:19:06 +1000

    Update index.js

--- src/muibook/pages/changelog/index.js
+++ src/muibook/pages/changelog/index.js
@@ -1,37 +1,15 @@
-import changelog from "../../../../CHANGELOG.md?raw";
-
 class ChangelogPage extends HTMLElement {
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    const styles = /*css*/ `
-      :host { display: block; }
-    
-      .config::part(display) {
-        grid-template-columns: 1fr;
-      }
+    this.attachShadow({ mode: "open" });
+  }
 
-      @media (min-width: 1230px) {
-        .config::part(display) {
-          grid-template-columns: minmax(0, 50ch) 400px;
-          gap: 7.2rem;
-          justify-content: space-between;
-          align-items: start;
-        }
-      }
+  async connectedCallback() {
+    const res = await fetch("/CHANGELOG.md");
+    const changelog = await res.text();
 
-      @media (min-width: 1390px) {
-        .config::part(display) {
-          grid-template-columns: minmax(0, 50ch) 500px;
-          gap: 9.6rem;
-        }
-      }
-
-    `;
-
-    shadowRoot.innerHTML = /*html*/ `
-      <style>${styles}</style>
-
+    this.shadowRoot.innerHTML = /*html*/ `
+      <style>:host { display:block; }</style>
       <story-template 
         title="Changelog"
         description="Keep track of updates, fixes, and new features for your components."
