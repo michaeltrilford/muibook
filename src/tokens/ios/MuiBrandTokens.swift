f72a02cf10a3e7c907c0e4373d97a56e2e07213a
Michael Trilford <trilfordm@gmail.com>
2026-06-14 22:03:59 +1000

    Update knowledge curation, rules, and composition examples

--- src/tokens/ios/MuiBrandTokens.swift
+++ src/tokens/ios/MuiBrandTokens.swift
@@ -2,7 +2,7 @@
 // MuiBrandTokens.swift
 //
 
-// Don't edit directly • Generated on Sun, 14 Jun 2026 11:26:17 GMT • muibook.com
+// Don't edit directly • Generated on Sun, 14 Jun 2026 12:01:25 GMT • muibook.com
 
 import UIKit
 
