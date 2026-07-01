fddf8e8d63a7e9efe52770bd9d1046a8097eaddf
Michael Trilford <trilfordm@gmail.com>
2026-07-01 11:11:23 +1000

    Drawer component update
    • Added '--drawer-height' to the Drawer component to allow for further customisation.

--- src/tokens/ios/MuiBrandTokens.swift
+++ src/tokens/ios/MuiBrandTokens.swift
@@ -2,7 +2,7 @@
 // MuiBrandTokens.swift
 //
 
-// Don't edit directly • Generated on Tue, 30 Jun 2026 13:22:36 GMT • muibook.com
+// Don't edit directly • Generated on Tue, 30 Jun 2026 19:47:08 GMT • muibook.com
 
 import UIKit
 
