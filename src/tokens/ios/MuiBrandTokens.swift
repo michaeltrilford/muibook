603414eab34106f04ae2c9a25b8c83686eebfde6
Michael Trilford <trilfordm@gmail.com>
2026-08-13 23:29:13 +1000

    Preparing v26.0.1

--- src/tokens/ios/MuiBrandTokens.swift
+++ src/tokens/ios/MuiBrandTokens.swift
@@ -2,7 +2,7 @@
 // MuiBrandTokens.swift
 //
 
-// Don't edit directly • Generated on Thu, 13 Aug 2026 13:25:36 GMT • muibook.com
+// Don't edit directly • Generated on Thu, 13 Aug 2026 13:28:19 GMT • muibook.com
 
 import UIKit
 
